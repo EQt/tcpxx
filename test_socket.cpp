e41a18cbbe608e91f0c8042cf92e57cfdf6437fe
Elias Kuthe <elias.kuthe@tu-dortmund.de>
2017-05-04 19:09:45 +0200

    udp when parameters provided

--- test_socket.cpp
+++ test_socket.cpp
@@ -3,10 +3,10 @@
 
 
 int
-main()
+main(int argc, char *argv[])
 {
     try {
-        bool udp = false;
+        bool udp = argc > 1;
         Socket sock ("localhost", 5555, true, udp);
         std::cout << sock.send("Hello via Socket\n")
                   << std::endl;
